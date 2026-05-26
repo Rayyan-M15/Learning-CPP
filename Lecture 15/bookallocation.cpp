03cac71c2b4c4f08f7299475e1243df5bd4c5f24
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-05-26 22:41:35 +0530

    improving the book allocation with explanation

--- Lecture 15/bookallocation.cpp
+++ Lecture 15/bookallocation.cpp
@@ -3,25 +3,27 @@
 
 using namespace std;
 
-bool isPossible(vector<int>& books, int students, int maxPages) {
+bool isPossible(vector<int>& arr, int n, int m, int maxpages) {
 
-    int studentCount = 1;
-    int currentPages = 0;
+    int studentcount = 1;
+    int currentpages = 0;
 
-    for(int i = 0; i < books.size(); i++) {
+    for(int i = 0; i < n; i++) {
 
-        if(books[i] > maxPages) {
+        if(arr[i] > maxpages) { //if 1 book has more pages than the highest one in the arr
             return false;
         }
 
-        if(currentPages + books[i] <= maxPages) {
-            currentPages += books[i];
+        if(currentpages + arr[i] <= maxpages){ //assgn to the current student
+            currentpages += arr[i];
         }
-        else {
-            studentCount++;
-            currentPages = books[i];
 
-            if(studentCount > students) {
+        else{
+            //incase all the books werent allocated we increase the students
+            studentcount++;
+            currentpages = arr[i];
+
+            if(studentcount>m){//if there are more students than there are books
                 return false;
             }
         }
@@ -30,39 +32,38 @@ bool isPossible(vector<int>& books, int students, int maxPages) {
     return true;
 }
 
-int allocateBooks(vector<int>& books, int students) {
 
-    int n = books.size();
-
-    if(students > n) {
+int findPages(vector<int>& arr, int n, int m) {
+    if(m>n){ //more students than books
         return -1;
     }
 
-    int low = books[0];
-    int high = 0;
+    int low = arr[0]; //just assuming
+    int high = 0; //just assuming
 
-    // Finding max element and sum manually
-    for(int i = 0; i < n; i++) {
-
-        if(books[i] > low) {
-            low = books[i];
+    for(int i = 0; i<n ; i++){
+        if(arr[i]>low){
+            low = arr[i]; // low = max number of pages in any book
         }
 
-        high += books[i];
+        high += arr[i]; // high = sum of all the pages in arr
     }
 
     int ans = -1;
 
-    while(low <= high) {
+    //binary search part
 
-        int mid = low + (high - low) / 2;
+    while(low<=high){
+        int mid = low + (high - low)/2;
 
-        if(isPossible(books, students, mid)) {
-            ans = mid;
-            high = mid - 1;
+        if(isPossible(arr, n, m, mid)){
+            ans = mid; //storig the answer in case it is the final ans
+
+            high = mid -1;//to check if a smller ans is possible
         }
-        else {
-            low = mid + 1;
+
+        else{
+            low = mid +1; //incase more than available students are required
         }
     }
 
@@ -71,10 +72,12 @@ int allocateBooks(vector<int>& books, int students) {
 
 int main() {
 
-    vector<int> books = {10, 20, 30, 40};
-    int students = 2;
+    vector<int> arr = {10, 20, 30, 40};
 
-    cout << allocateBooks(books, students)<<endl;
+    int n = arr.size(); // Number of books
+    int m = 2;          // Number of students
+
+    cout << findPages(arr, n, m) << endl;
 
     return 0;
 }
