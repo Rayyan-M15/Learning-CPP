bf32a9feb9e208dec50858dd1ed5070144a7b686
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-05-30 15:26:42 +0530

    Lecture 20 revision

--- Lecture 20/leetcode283.cpp
+++ Lecture 20/leetcode283.cpp
@@ -1,21 +1,21 @@
 // leetcode 283
 
 //my solution
-class Solution {
-public:
-    void moveZeroes(vector<int>& arr) {
-        int n = arr.size();
-        int j = 0;
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& arr) {
+//         int n = arr.size();
+//         int j = 0;
         
-        for(int i =0; i< n; i++){
+//         for(int i =0; i< n; i++){
 
-            if(arr[i]!=0){
-                swap(arr[i], arr[j]);
-                j++;
-            }
-        }
-    }
-};
+//             if(arr[i]!=0){
+//                 swap(arr[i], arr[j]);
+//                 j++;
+//             }
+//         }
+//     }
+// };
 
 
 //solution 2
