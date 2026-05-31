986bba4a57d5f9636c0a0dce79beddd5e8de9103
Rayyan Rasool Mir <rayyanrasoolmir@gmail.com>
2026-06-01 01:08:02 +0530

    GFG problem Lec 22

--- Lecture 22/gfgprob.cpp
+++ Lecture 22/gfgprob.cpp
@@ -12,3 +12,35 @@ Explanation: 't' and 'u' are the characters with the same frequency, but 't' is 
 Constraints:
 1 ≤ |s| ≤ 100
 */
+
+#include <iostream>
+using namespace std;
+char getMaxOccuringChar(string str) {
+    char freq[26]= {0}; //a->0 b->1 ... z ->25
+    
+    for(int i = 0; str[i] != '\0'; i++){
+        char ch = str[i];
+        int number = ch - 'a';
+        
+        freq[number]++; //for the character that exists in the string the number goes up
+    }
+    
+    int maxfreq = 0, ans = 0;
+    for(int j = 0; j<26; j++){
+        if(maxfreq < freq[j]){
+            ans = j;
+            maxfreq = freq[j];
+        }
+    }
+    
+    char final = ans + 'a';
+    return final;
+    
+}
+
+int main()
+{
+    string s = "outp u t";
+    cout<< getMaxOccuringChar(s);
+    return 0;
+}
--- Lecture 22/str.cpp
+++ Lecture 22/str.cpp
@@ -5,15 +5,39 @@ int main()
     string s1 = "Rayyan";
     char s2[] = {'R', 'a', 'y', 'y', 'a', 'n', '\0'};
 
-    for (int i = 0; i < s1.size(); i++)
-    {
-        cout<<s1[i];
-    }cout<<endl;
-
+    
     for (int i = 0; i < sizeof(s2)/sizeof(s2[0]); i++)
     {
         cout<<s2[i];
     }cout<<endl;
     
+    cout<<s1.length()<<endl;
+    
+    s1.push_back('R');
+    
+    cout<<"After PushBack: ";
+    for (int i = 0; i < s1.size(); i++)
+    {
+        cout<<s1[i];
+    }cout<<endl;
+    
+    
+    cout<<"After Appending: ";
+    s1.append("Mir");
+    
+    for (int i = 0; i < s1.size(); i++)
+    {
+        cout<<s1[i];
+    }cout<<endl;
+    
+    cout<<"After popback: ";
+
+    s1.pop_back();
+    
+    for (int i = 0; i < s1.size(); i++)
+    {
+        cout<<s1[i];
+    }cout<<endl;
+
     return 0;
 }
